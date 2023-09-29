f62f2079938ce9dcaa344c4072ccb1da0c20fdd5
Fazzy F.r <fazzyfitraturrahman93@gmail.com>
2023-09-30 06:16:59 +0700

    homework week 7

---
+++ bangun_datar/persegi.js
@@ -0,0 +1,12 @@
+function luas(s) {
+  return s * s;
+}
+
+function keliling(s) {
+  return 4 * s;
+}
+
+module.exports = {
+  luas,
+  keliling,
+};
---
+++ bangun_datar/persegi_panjang.js
@@ -0,0 +1,12 @@
+function luas(p, l) {
+  return p * l;
+}
+
+function keliling(p, l) {
+  return 2 * (p + l);
+}
+
+module.exports = {
+  luas,
+  keliling,
+};
